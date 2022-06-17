bb55ae8542caceca3d5ede0abf2802853854f5e7
NovaPigeon <94160042+NovaPigeon@users.noreply.github.com>
2022-06-17 09:08:30 +0800

    增加了开场图片（或许还可以加个开场动画？）

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include "gamebeginwindow.h"
 #include <QApplication>
-
+#include <QSplashScreen>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setWindowIcon(QIcon(":/Image/icon"));
+    //增加打开游戏时的图片特效
+    QSplashScreen *splash = new QSplashScreen;
+    splash->setPixmap(QPixmap(":/Image/openScene.png"));
+    splash->show();
     gameBeginWindow w;
-    w.show();
+    QTimer::singleShot(1000,splash,SLOT(close()));
+    QTimer::singleShot(2000,&w,SLOT(show()));
 
     return a.exec();
 }
